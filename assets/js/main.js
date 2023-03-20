72501a5ffb7b57512383ac6971625a944ab359dc
devancadman@outlook.com <devancadman@outlook.com>
2023-03-20 08:41:49 +0000

    Corrected and added JS for Slider Animation image delay

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,3 @@
-var myCarousel = document.querySelector('#myCarousel');
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 8000 // set interval to 15 seconds
-});
-
-// Show the first slide initially
-carousel.to(0);
-
-// Create an array of all the modals
-var modals = document.querySelectorAll('.modal');
-
-// Loop through each modal
-modals.forEach(function(modal, index) {
-  // Show the modal when the corresponding slide is shown
-  myCarousel.addEventListener('slide.bs.carousel', function(event) {
-    if (event.to === index) {
-      var modalId = modal.getAttribute('id');
-      var modalInstance = new bootstrap.Modal(document.querySelector(`#${modalId}`));
-      setTimeout(function() {
-        modalInstance.show();
-        carousel.pause();
-      }, 3000); // show modal after 4 seconds
-    }
-  });
-
-  // Hide the modal and restart the carousel when the modal is closed
-  modal.addEventListener('hidden.bs.modal', function() {
-    carousel.cycle();
-  });
-});
-
-
-
 // Get all the slides
 var slides = document.querySelectorAll('.carousel-item');
 
