c5f4f35da4c99a1e6bc4108e99a6128c5f942dd4
devancadman@outlook.com <devancadman@outlook.com>
2023-03-20 10:30:39 +0000

    Added back to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,28 @@ myCarousel.addEventListener('slid.bs.carousel', function() {
 });
 
 
+/* Source - https://stackoverflow.com/questions/14004117/create-div-and-append-div-dynamically*/
+window.addEventListener('load', function() {
+  /*Add back to top button*/
+  const toastBackToTop = document.createElement('div');
+  toastBackToTop.classList.add('back-to-top');
+  document.body.appendChild(toastBackToTop);
+
+  /* Show and hide button based on scroll location - Source: https://stackoverflow.com/questions/28547200/show-hide-menu-based-on-scroll-position & https://codepen.io/matthewcain/pen/ZepbeR*/
+  window.addEventListener('scroll', function() {
+      if (window.pageYOffset > 20) {
+          toastBackToTop.classList.add('show');
+      } else {
+          toastBackToTop.classList.remove('show');
+      }
+  });
+
+  /* Functionality to take user back to the top of the site */
+  document.body.addEventListener('click', function(event) {
+      if (event.target.classList.contains('back-to-top')) {
+          window.scrollTo({
+              top: 0,
+              behavior: 'smooth'
+          });
+      }
+  });
